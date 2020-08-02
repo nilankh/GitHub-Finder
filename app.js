517581b306ca0aa2cfa142db62167c5703542b4c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-02 17:59:32 +0530

    done show alert message

--- app.js
+++ app.js
@@ -8,21 +8,24 @@ const ui = new UI;
 const searchUser = document.getElementById("searchUser");
 
 // Search input event listener
-searchUser.addEventListener("keyup", (e) => {
-  // Get input text
-  const userText = e.target.value;
-
-  if (userText !== "") {
-    // Make http call
-    github.getUser(userText).then((data) => {
-      if (data.profile.message === "Not Found") {
-        // Show alert
-      } else {
-        // Show profile
-        ui.showProfile(data.profile);
-      }
-    });
-  } else {
-    // Clear profile
-  }
-});
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+  
+    if(userText !== ''){
+     // Make http call
+     github.getUser(userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          // Show alert
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          // Show profile
+          ui.showProfile(data.profile);
+        }
+      })
+    } else {
+      // Clear profile
+      ui.clearProfile();
+    }
+  }); 
