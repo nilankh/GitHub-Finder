6dd0c3de9ef3aabd15ab3010d9436bc6fdcedcc8
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-02 17:37:25 +0530

    done display the profile

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init Github
 const github = new Github();
 
+// Init UI
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
@@ -16,6 +19,7 @@ searchUser.addEventListener("keyup", (e) => {
         // Show alert
       } else {
         // Show profile
+        ui.showProfile(data.profile);
       }
     });
   } else {
